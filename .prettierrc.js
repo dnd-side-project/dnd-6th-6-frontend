5e64abaf2bd26fa61069bf77a40cb6df14532084
LeeJaeHoon <dlwogns3413@naver.com>
2022-01-13 15:30:16 +0900

    Develop Setting

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  singleQuote: true,
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: 'all',
+  printWidth: 120,
+  arrowParens: 'always',
+};
+
+// singleQuote : single 쿼테이션 사용 여부
+// semi : 세미콜론 사용 여부
+// useTabs : 탭 사용 여부
+// tabWidth : 탭 너비
+// trailingComma : 여러 줄을 사용할 때, 후행 콤마 사용 방식
+// printWidth : 줄 바꿈 할 폭 길이
+// arrowParens : 화살표 함수 괄호 사용 방식
