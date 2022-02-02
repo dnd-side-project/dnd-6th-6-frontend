4696880b8a4c823c4ec2aaf4df25653eeae56488
dongyeopca <ckehdduq95@gmail.com>
2022-02-02 17:30:36 +0900

    feat-issue#13-MolTextInput

---
+++ src/components/UI/molecules/TextInput/TextInput.tsx
@@ -0,0 +1,25 @@
+import { UseFormRegisterReturn } from 'react-hook-form';
+import Label from '../../atoms/Label/Label';
+import Input from '../../atoms/Input/Input';
+import Message from '../../atoms/Message/Message';
+import { StyledTextInput } from './TextInputStyles';
+import { InputType } from '../../atoms/Input/Input';
+
+export interface IMolTextInputProps {
+  labelText: string;
+  register?: UseFormRegisterReturn;
+  message?: string;
+  type?: InputType;
+}
+
+const TextInput = (props: IMolTextInputProps) => {
+  return (
+    <StyledTextInput>
+      <Label>{props.labelText}</Label>
+      <Input register={props.register} type={props.type}></Input>
+      <Message className="error">{props.message}</Message>
+    </StyledTextInput>
+  );
+};
+
+export default TextInput;
