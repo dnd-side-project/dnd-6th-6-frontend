59425a322c45dbed9e1dbb3d1bf5a111ae13d47f
LeeJaeHoon <dlwogns3413@naver.com>
2022-02-02 15:03:39 +0900

    fix: storybook bug

---
+++ src/components/UI/atoms/Input/Input.tsx
@@ -0,0 +1,9 @@
+import { StyledInput } from './InputStyled';
+
+export interface IAtomInputProps {}
+
+const Input = (props: IAtomInputProps) => {
+  return <StyledInput></StyledInput>;
+};
+
+export default Input;
