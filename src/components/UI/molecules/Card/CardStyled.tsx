ea8a8d74074c95a85459a6cfba01857ca9d1aff1
LeeJaeHoon <dlwogns3413@naver.com>
2022-02-03 17:25:49 +0900

    feat: type-issue/#29-molecules/Card clear

---
+++ src/components/UI/molecules/Card/CardStyled.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const StyledCard = styled.div`
+  width: 100%;
+  height: 60px;
+  /* max-height: auto; */
+  padding: 10px;
+  display: flex;
+  border: 1px solid #dfdfdf;
+  border-radius: 10px;
+  align-items: center;
+  gap: 20px;
+`;
