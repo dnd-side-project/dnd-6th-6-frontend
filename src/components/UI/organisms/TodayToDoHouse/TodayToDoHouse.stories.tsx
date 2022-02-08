cfe234078df813bf934e85bb22405c32ed7dca57
LeeJaeHoon <dlwogns3413@naver.com>
2022-02-09 04:47:41 +0900

    feat: 하우스 할 일 현황 organism clear

---
+++ src/components/UI/organisms/TodayToDoHouse/TodayToDoHouse.stories.tsx
@@ -0,0 +1,29 @@
+import { Meta, Story } from '@storybook/react';
+import TodayToDoHouse, { IOrgTodayToDoHouseProps } from './TodayToDoHouse';
+
+export default {
+  TodayToDoHouse: 'origanisms/TodayToDoHouse',
+  component: TodayToDoHouse,
+  parameters: {},
+  argTypes: {},
+} as Meta;
+
+const Template: Story<IOrgTodayToDoHouseProps> = (args) => (
+  <>
+    <div
+      style={{
+        width: '375px',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        backgroundColor: '#F8F9FD',
+      }}
+    >
+      <TodayToDoHouse {...args}></TodayToDoHouse>
+    </div>
+  </>
+);
+
+export const Default = Template.bind({});
+Default.parameters = {};
+Default.args = {};
