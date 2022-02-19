e32838bf451373c51395e5378c0d3cef9825de22
LeeJaeHoon <dlwogns3413@naver.com>
2022-02-19 23:19:07 +0900

    feat: clear feedback api

---
+++ src/apis/feedback.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://미정';
+
+axios.defaults.baseURL = BASE_URL;
+
+// 피드백 보내기
+export const sendFeedbackAPI = ({ choreId, content }: { choreId: number; content: string }) => {
+  return axios.post(`${BASE_URL}/chores/${choreId}/feedbacks`, { content }).then((response) => response.data);
+};
+
+// 피드백 목록
+export const getFeedbackAPI = (choreId: number) => {
+  return axios.get(`${BASE_URL}/chores/${choreId}/feedbacks`).then((response) => response.data);
+};
+
+// 피드백 상세보기
+export const getDetailFeedbackAPI = (choreId: number, feedbackId: number) => {
+  return axios.get(`${BASE_URL}/chores/${choreId}/feedbacks/${feedbackId}`).then((response) => response.data);
+};
